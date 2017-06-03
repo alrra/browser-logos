01d2d93da2b4904b304e58404d82093402197eac
Cătălin Mariș <alrraa@gmail.com>
2017-06-03 08:56:57 +0300

    Refactor and improve `check-links.sh`

--- scripts/check-links.sh
+++ scripts/check-links.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")/.." \
+    || exit 1
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-find ../ -name "*.md" \
-    | xargs awesome_bot --allow-dupe \
-                        --allow-redirect \
-                        --set-timeout 150 \
-                        --white-list "https://web.archive.org/web"
+declare -r -a IGNORED_URLS=(
+    "https://github.com/alrra/browser-logos/commit/4406d8a2ef0f9cf1fd91cf1c9b438b2096a51bba"
+    "https://web.archive.org/web"
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+find .  -name "*.md" \
+        -not -path "./node_modules/*" \
+        -exec awesome_bot \
+                --allow-dupe \
+                --allow-redirect \
+                --set-timeout 150 \
+                --white-list "$(IFS=,;printf "%s" "${IGNORED_URLS[*]}")" \
+                {} +
