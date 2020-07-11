a9685a64258dc6308b01968e1cdad3e9d1c754ec
Cătălin Mariș <alrraa@gmail.com>
2020-07-10 17:32:20 -0700

    🏗 Exclude changelogs from link checking
    GitHub has a request rate limit, so after `markdown-link-check`
    checks a few of the GitHub links, all the other ones will return
    with `429 Too Many Requests`.
    
    Ref / See also: https://github.com/tcort/markdown-link-check/issues/85

--- scripts/check-links.sh
+++ scripts/check-links.sh
@@ -6,7 +6,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.." \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 declare exitCode=0
-declare markdownFiles=$(find . -name "*.md" -not -path "./node_modules/*");
+declare markdownFiles=$( \
+    find . \
+        -name "*.md" \
+        -not -name "CHANGELOG.md" \
+        -not -path "./changelogs/*" \
+        -not -path "./node_modules/*" \
+);
 
 for file in $markdownFiles; do
     markdown-link-check -pv "$file" \
